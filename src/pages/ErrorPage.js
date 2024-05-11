8100bc1f1a4e5b891353094f5e94f57985cc87a7
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-05-11 12:10:30 +0300

    routes

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import { Navbar } from "../components/NavBar";
+
+
+function ErrorPage() {
+  return (
+    <>
+    <nav>
+        <NavBar />
+      </nav>
+      <div>
+        <h1>Oops! Looks like something went wrong.</h1>
+      </div>
+    </>
+  )
+}
+export default ErrorPage;
